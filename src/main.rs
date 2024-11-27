3b4d5fcafd06e7417483096100dde39ba0a730e9
PrsRicky15 <prashantsingh1993@gmail.com>
2024-11-27 16:45:46 +0200

    3d coordinate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod basiclearn;
 mod math_vector;
-
+use math_vector::math_vector::Coord3d;
 fn main() {
    basiclearn::control_flow::gaussian_basis();
+   let P1 = Coord3d::new(0,0,0);
 }
--- src/math_vector.rs
+++ src/math_vector.rs
@@ -1,67 +1,116 @@
-pub mod vectors{
-    use std::ops::{Add, Mul};
+pub mod math_vector {
+    use std::ops::{Add, Sub, Mul};
+
     #[derive(Debug, Copy, Clone)]
-    struct Coord3d<U> {
+    pub struct Coord3d<U> {
         x: U,
         y: U,
         z: U,
     }
 
     impl<U> Coord3d<U> {
-        fn new(x: U, y: U, z:U) -> Coord3d<U> {
-            Coord3d {x, y, z}
+        pub fn new(x: U, y: U, z: U) -> Self {
+            Self { x, y, z }
         }
 
-        fn shift_all<A>(&self, a: A) -> Coord3d<U> where U: Add<A, Output=U> + Copy, A: Copy{
-            Coord3d {
+        pub fn shift_all(&self, a: U) -> Self
+        where
+            U: Add<Output = U> + Copy,
+        {
+            Self {
                 x: self.x + a,
                 y: self.y + a,
-                z: self.z + a
+                z: self.z + a,
             }
         }
 
-        fn shift_x<A>(&self, a: A) -> Coord3d<U> where U: Add<A, Output=U> + Copy, A: Copy{
-            Coord3d {
+        pub fn shift_x(&self, a: U) -> Self
+        where
+            U: Add<Output = U> + Copy,
+        {
+            Self {
                 x: self.x + a,
                 y: self.y,
-                z: self.z
+                z: self.z,
             }
         }
 
-        fn shift_y<A>(&self, a: A) -> Coord3d<U> where U: Add<A, Output=U> + Copy,A: Copy{
-            Coord3d {
+        pub fn shift_y(&self, a: U) -> Self
+        where
+            U: Add<Output = U> + Copy,
+        {
+            Self {
                 x: self.x,
                 y: self.y + a,
-                z: self.z
+                z: self.z,
             }
         }
 
-        fn shift_z<A>(&self, a: A) -> Coord3d<U> where U: Add<A, Output=U> + Copy, A: Copy{
-            Coord3d {
+        pub fn shift_z(&self, a: U) -> Self
+        where
+            U: Add<Output = U> + Copy,
+        {
+            Self {
                 x: self.x,
                 y: self.y,
-                z: self.z + a
+                z: self.z + a,
             }
         }
 
-        fn scale<A>(&self, a: A) -> Coord3d<U> where U: Mul<A, Output=U> + Copy, A: Copy{
-            Coord3d {
+        pub fn scale(&self, a: U) -> Self
+        where
+            U: Mul<Output = U> + Copy,
+        {
+            Self {
                 x: self.x * a,
                 y: self.y * a,
-                z: self.z * a
+                z: self.z * a,
             }
         }
 
-        fn add_coord<A>(&self, coord3d: Coord3d<A>) -> Coord3d<U> where U: Add<A, Output=U> + Copy{
-            Coord3d {
+        pub fn add_coord(&self, coord3d: Coord3d<U>) -> Self
+        where
+            U: Add<Output = U> + Copy,
+        {
+            Self {
                 x: self.x + coord3d.x,
                 y: self.y + coord3d.y,
-                z: self.z + coord3d.z
+                z: self.z + coord3d.z,
             }
         }
 
-        fn origin()->Coord3d<U>{
-            Coord3d{x:0, y:0, z:0}
+        pub fn origin() -> Self
+        where
+            U: Default,
+        {
+            Self {
+                x: U::default(),
+                y: U::default(),
+                z: U::default(),
+            }
+        }
+
+        pub fn distance(&self, coord3d: Coord3d<U>) -> U
+        where
+            U: Sub<Output = U> + Add<Output = U> + Copy + Mul<Output = U>,
+        {
+            let dx = self.x - coord3d.x;
+            let dy = self.y - coord3d.y;
+            let dz = self.z - coord3d.z;
+            dx * dx + dy * dy + dz * dz
+        }
+    }
+
+    impl<U> Default for Coord3d<U>
+    where
+        U: Default,
+    {
+         fn default() -> Self {
+            Self {
+                x: U::default(),
+                y: U::default(),
+                z: U::default(),
+            }
         }
     }
 }
